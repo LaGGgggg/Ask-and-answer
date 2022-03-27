b55a9c418c9ae48a4f299ac80000d8d27276223e
LaGGgggg <Poni22poni23@yandex.ru>
2022-03-27 19:32:57 +0400

    repair code

--- accounts_class_mysql.py
+++ accounts_class_mysql.py
@@ -7,16 +7,26 @@ cur = conn.cursor()
 
 cur.execute('SHOW DATABASES')
 
-if not cur.fetchall():
+row = cur.fetchall()
+
+if 'accounts_bd_mysql' not in row[0]:
 
     cur.execute('CREATE DATABASE accounts_bd_mysql')
 
     conn.commit()
 
+cur.close()
+
+conn.close()
+
+conn = connect(host='localhost', user='root', password='123asd159ZXC', database='accounts_bd_mysql')
+
+cur = conn.cursor()
+
 cur.execute('''CREATE TABLE IF NOT EXISTS accounts(
-            id INT AUTO_INCREMENT PRIMARY KEY,
+            id INT AUTO_INCREMENT PRIMARY KEY, 
             cash INT, 
-            login VARCHAR(100),
+            login VARCHAR(100), 
             password VARCHAR(100)
             )''')  # TODO разнести в две отдельные таблицы
 
