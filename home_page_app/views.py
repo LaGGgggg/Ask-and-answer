1bec8a56d52f05d8441472e548046eb827fc3704
LaGGgggg <Poni22poni23@yandex.ru>
2022-03-30 17:43:42 +0400

    Add django!
    Add django, but not add account_class_mysql functional for it.

---
+++ home_page_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h1>Hello chelad!)</h1>')
+
+
+def about(request):
+    return HttpResponse('<h2>About us.</h2>')
+
+
+def contact(request):
+    return HttpResponse('<h2>Contact with us.</h2>')
+
+
+def user_info(request, user_id, user_name):
+    return HttpResponse('<h2>User name: {0}<br>Id: {1}</h2>'.format(user_name, user_id))
+
+
+def error(request):
+    return HttpResponse('<h1>Incorrect web-address.</h1>')
