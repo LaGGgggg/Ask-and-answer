cbf544b2c83cf49681dee36596fa7cd2cd65f073
LaGGgggg <Poni22poni23@yandex.ru>
2022-05-24 15:20:10 +0400

    Some repairs and improvements.
    Now no authenticated users don't see like buttons.
    Rewrite all switch/case constractions to if/elif.(Because not all support python 3.10.)
    Repair is_ajax system.(Now ajax requests are definded correctly.)

--- home_page_app/views.py
+++ home_page_app/views.py
@@ -14,97 +14,89 @@ def view_main(request):
 
     latest_questions = Questions.objects.order_by('-pub_date')[:5]
 
-    match request.method:
+    if request.method == 'GET':
 
-        case 'GET':
+        return render(request, 'home_page_app/index.html', {
+            'latest_questions': latest_questions,
+            'form': SearchQuestionForm(),
+        })
+
+    elif request.method == 'POST':
+
+        search_question_form = SearchQuestionForm(request.POST)
+
+        if search_question_form.is_valid():
+
+            found_questions = \
+                Questions.objects.filter(title__contains=search_question_form.cleaned_data['content'])
+
+            if found_questions:
+
+                return render(request, 'home_page_app/index.html', {
+                    'latest_questions': latest_questions,
+                    'form': search_question_form,
+                    'found_questions': found_questions,
+                })
+
+            else:
+
+                return render(request, 'home_page_app/index.html', {
+                    'latest_questions': latest_questions,
+                    'form': search_question_form,
+                })
+
+        else:
 
             return render(request, 'home_page_app/index.html', {
                 'latest_questions': latest_questions,
                 'form': SearchQuestionForm(),
             })
 
-        case 'POST':
-
-            search_question_form = SearchQuestionForm(request.POST)
-
-            if search_question_form.is_valid():
-
-                found_questions = \
-                    Questions.objects.filter(title__contains=search_question_form.cleaned_data['content'])
-
-                if found_questions:
-
-                    return render(request, 'home_page_app/index.html', {
-                        'latest_questions': latest_questions,
-                        'form': search_question_form,
-                        'found_questions': found_questions,
-                    })
-
-                else:
-
-                    return render(request, 'home_page_app/index.html', {
-                        'latest_questions': latest_questions,
-                        'form': search_question_form,
-                    })
-
-            else:
-
-                return render(request, 'home_page_app/index.html', {
-                    'latest_questions': latest_questions,
-                    'form': SearchQuestionForm(),
-                })
-
-        case _:
-
-            return render(request, 'error.html')
+    else:
+        return render(request, 'error.html')
 
 
 def add_question(request):
 
-    match request.method:
+    if request.method == 'POST':
 
-        case 'POST':
+        make_question_form = MakeQuestionForm(request.POST)
 
-            make_question_form = MakeQuestionForm(request.POST)
+        if make_question_form.is_valid():
 
-            if make_question_form.is_valid():
+            try:
 
-                try:
+                Questions.objects.create(
+                    title=make_question_form.cleaned_data['title'],
+                    content=make_question_form.cleaned_data['content'],
+                    user=request.user,
+                )
 
-                    Questions.objects.create(
-                        title=make_question_form.cleaned_data['title'],
-                        content=make_question_form.cleaned_data['content'],
-                        user=request.user,
-                    )
+                # cash by create question:
 
-                    # cash by create question:
+                user = Profile.objects.get(user_id=request.user.id)
 
-                    user = Profile.objects.get(user_id=request.user.id)
+                user.cash += 10
 
-                    user.cash += 10
+                user.save()
 
-                    user.save()
+                return render(request, 'home_page_app/create_question_successfully.html')
 
-                    return render(request, 'home_page_app/create_question_successfully.html')
+            except IntegrityError:
 
-                except IntegrityError:
+                return render(request, 'home_page_app/create_question.html', {
+                    'form': make_question_form,
+                    'comment': 'Not unique text!',
+                })
 
-                    return render(request, 'home_page_app/create_question.html', {
-                        'form': make_question_form,
-                        'comment': 'Not unique text!',
-                    })
-
-            else:
-
-                return render(request, 'home_page_app/create_question.html', {'form': MakeQuestionForm()})
-
-        case 'GET':
-
+        else:
             return render(request, 'home_page_app/create_question.html', {'form': MakeQuestionForm()})
 
-        case _:
+    elif request.method == 'GET':
+        return render(request, 'home_page_app/create_question.html', {'form': MakeQuestionForm()})
 
-            return render(request, 'error.html')
+    else:
+        return render(request, 'error.html')
 
 
 def view_question(request, question_id):
@@ -126,96 +118,92 @@ def view_question(request, question_id):
         'id': question_id,
     }
 
-    if request.method == 'POST' and request.accepts('ajax_like'):
+    # Ajax request:
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == 'POST':
 
-        match request.POST.get('object_type'):
+        if request.POST.get('object_type') == 'question':
 
-            case 'question':
-
-                if question.likes.filter(id=user.id).exists():
-                    question.likes.remove(user)
-
-                else:
-                    question.likes.add(user)
-
-                return HttpResponse(
-                    json.dumps({
-                        'comments_likes_values': question.total_likes(),
-                    }),
-                    content_type='application/json',
-                )
-
-            case 'comment':
-
-                comment_id = request.POST.get('object_id')
-                comment = Comments.objects.get(id=comment_id)
-
-                if comment.likes.filter(id=user.id).exists():
-                    comment.likes.remove(user)
-
-                else:
-                    comment.likes.add(user)
-
-                return HttpResponse(
-                    json.dumps({
-                        'comments_likes_values': comment.total_likes(),
-                    }),
-                    content_type='application/json',
-                )
-
-    match request.method:
-
-        case 'POST':
-
-            make_comment_form = MakeCommentForm(request.POST)
-
-            if make_comment_form.is_valid():
-
-                if Comments.objects.filter(content=make_comment_form.cleaned_data['content']).exists():
-
-                    return render(request, 'home_page_app/view_question.html', {
-                        'question_data': question_data,
-                        'form': MakeCommentForm(),
-                        'comment': 'Not unique text!',
-                    })
-
-                else:
-
-                    Comments.objects.create(
-                        question_id=question_id,
-                        content=make_comment_form.cleaned_data['content'],
-                        user=request.user,
-                    )
-
-                    # cash by create comment:
-
-                    user = Profile.objects.get(user_id=request.user.id)
-
-                    user.cash += 5
-
-                    user.save()
-
-                    question_data['comments'] = get_comments
-
-                    return render(request, 'home_page_app/view_question.html', {
-                        'question_data': question_data,
-                        'form': MakeCommentForm(),
-                    })
+            if question.likes.filter(id=user.id).exists():
+                question.likes.remove(user)
 
             else:
+                question.likes.add(user)
+
+            return HttpResponse(
+                json.dumps({
+                    'comments_likes_values': question.total_likes(),
+                }),
+                content_type='application/json',
+            )
+
+        elif request.POST.get('object_type') == 'comment':
+
+            comment_id = request.POST.get('object_id')
+            comment = Comments.objects.get(id=comment_id)
+
+            if comment.likes.filter(id=user.id).exists():
+                comment.likes.remove(user)
+
+            else:
+                comment.likes.add(user)
+
+            return HttpResponse(
+                json.dumps({
+                    'comments_likes_values': comment.total_likes(),
+                }),
+                content_type='application/json',
+            )
+
+    elif request.method == 'POST':
+
+        make_comment_form = MakeCommentForm(request.POST)
+
+        if make_comment_form.is_valid():
+
+            if Comments.objects.filter(content=make_comment_form.cleaned_data['content']).exists():
+
+                return render(request, 'home_page_app/view_question.html', {
+                    'question_data': question_data,
+                    'form': MakeCommentForm(),
+                    'comment': 'Not unique text!',
+                })
+
+            else:
+
+                Comments.objects.create(
+                    question_id=question_id,
+                    content=make_comment_form.cleaned_data['content'],
+                    user=request.user,
+                )
+
+                # cash by create comment:
+
+                user = Profile.objects.get(user_id=request.user.id)
+
+                user.cash += 5
+
+                user.save()
+
+                question_data['comments'] = get_comments
 
                 return render(request, 'home_page_app/view_question.html', {
                     'question_data': question_data,
                     'form': MakeCommentForm(),
                 })
 
-        case 'GET':
+        else:
 
             return render(request, 'home_page_app/view_question.html', {
                 'question_data': question_data,
                 'form': MakeCommentForm(),
             })
 
-        case _:
+    elif request.method == 'GET':
 
-            return render(request, 'error.html')
+        return render(request, 'home_page_app/view_question.html', {
+            'question_data': question_data,
+            'form': MakeCommentForm(),
+        })
+
+    else:
+        return render(request, 'error.html')
