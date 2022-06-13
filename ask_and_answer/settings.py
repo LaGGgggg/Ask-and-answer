663624842fed1716932abc2abcaea2f1b62c98ef
LaGGgggg <Poni22poni23@yandex.ru>
2022-06-13 11:16:31 +0400

    Temporary change DEBUG.

--- ask_and_answer/settings.py
+++ ask_and_answer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0jucsw#b0283hudro0iqtr37ql%@_ud@&v^xt=rg7ksee6nkc*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
