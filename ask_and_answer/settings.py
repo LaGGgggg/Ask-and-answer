8998213ae59d9b817796e9c3469d62bba6541c49
LaGGgggg <Poni22poni23@yandex.ru>
2022-06-13 11:21:30 +0400

    Added database HOST to environment variables.

--- ask_and_answer/settings.py
+++ ask_and_answer/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +94,7 @@ DATABASES = {
         'NAME': 'ask_and_answer_database',
         'USER': 'postgres',
         'PASSWORD': '123asd159ZXC',
-        'HOST': '.herokuapp.com',
+        'HOST': env('HOST'),
         'PORT': '5432',
     }
 }
