1408e2d65a5b7226c2dacb99f79e837bd81e6c1c
LaGGgggg <Poni22poni23@yandex.ru>
2022-06-18 16:01:53 +0400

    Try solve Heroku deploy problem part 3

--- ask_and_answer/settings/prod.py
+++ ask_and_answer/settings/prod.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.joinpath('static')
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR.joinpath('static'),
